73939dc093c026438b4439896c43287dc4ce1fad
gmartirosyan <grigor.martirosyan@jambit.com>
2022-12-08 19:17:06 +0400

    node to 16 again 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://localhost:8080'
+    apiBaseUrl: 'http://awseb-AWSEB-7S55ZLS7NXGY-640238258.us-west-1.elb.amazonaws.com'
   };
